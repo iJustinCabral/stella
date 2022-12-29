f3c4ed38b45ce8ab8cb781b69088590ee23a14da
Justin Cabral <ijustincabral@gmail.com>
2022-12-29 10:34:42 -0500

    Initial Commit

---
+++ Stella/StellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellaApp.swift
+//  Stella
+//
+//  Created by Justin Cabral on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct StellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
